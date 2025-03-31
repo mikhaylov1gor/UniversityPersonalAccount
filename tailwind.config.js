63ea57e5272eb4e926f5f2b399e7c6aca7347d02
Mikhaylov Igor <mikhaylov61180@gmail.com>
2025-03-31 22:46:28 +0700

    Add profile page layout and localization

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
