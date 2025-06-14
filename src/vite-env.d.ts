68fdd5eb9ce9e7a260013aa125e5cb88cc5f59f5
Mikhaylov Igor <mikhaylov61180@gmail.com>
2025-06-15 01:11:52 +0700

    обновил конфиг

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMeta {
+    readonly globEager: (
+        pattern: string
+    ) => Record<string, { default: never } | never>;
+    readonly glob: (
+        pattern: string,
+        options?: { eager?: boolean; import?: string }
+    ) => Record<string, never>;
+}
