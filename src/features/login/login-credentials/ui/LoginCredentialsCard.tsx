a9f58dda3a757d3312e343ef6cce097549c9a71d
Mikhaylov Igor <mikhaylov61180@gmail.com>
2025-06-19 09:12:19 +0700

    фикс бага

--- src/features/login/login-credentials/ui/LoginCredentialsCard.tsx
+++ src/features/login/login-credentials/ui/LoginCredentialsCard.tsx
@@ -51,7 +51,7 @@ export function LoginCredentialsCard(){
             <div className="w-full md:w-1/2 bg-white rounded-xl shadow-lg p-8">
                 <div className="relative">
                     <div style={{margin: '2rem'}}>
-                        <h1 className="text-2xl font-semibold text-center mb-6">{t("loginPage.title")}</h1>
+                        <h1 className="text-2xl text-center mb-6">{t("loginPage.title")}</h1>
                     </div>
                 </div>
 
